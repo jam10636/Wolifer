821fdc620fdc61701b391d2c35f410566295190a
Meng Tsou <jam10636@gmail.com>
2018-07-13 22:20:01 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+import In from './workthing/mainpage'
+ReactDOM.render(<In />,
+  document.getElementById('root'));
